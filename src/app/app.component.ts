d86e7397c2dd0f6fe8cf35d4127f5c18e3a4a17b
Boliubash Pavlo <5avel@hotmail.com>
2023-05-17 20:55:43 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {  } from '@ngrx/effects/src/utils';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { decrement, increment, reset } from './counter.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngrxSimple';
+
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>){
+    this.count$ = store.select('count');
+  }
+
+  clear() {
+    this.store.dispatch(reset());
+  }
+  decrease() {
+    this.store.dispatch(decrement());
+  }
+  increase() {
+    this.store.dispatch(increment());
+  }
 }
